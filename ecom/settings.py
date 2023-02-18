6d98bc2eda201541ae05e363c629f7b4e0ae2ac6
sarwars <alltime.id.master@gmail.com>
2023-02-19 01:05:58 +0600

    Product rendered

--- ecom/settings.py
+++ ecom/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR,]
-
+MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
