c4212620711a4e47620e0bdd72301800e9e3261d
sarwars <alltime.id.master@gmail.com>
2023-03-03 12:37:11 +0600

    unauthorized user order

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,13 +8,38 @@ for (var i=0; i < updateBtns.length; i++){
     
         console.log('user', user)
         if(user === 'AnonymousUser'){
-            console.log('User is not authenticated')
+            addCookieItem(productId, action)
         }else{
             updateUserOrder(productId, action)
         }
     })
 }
 
+function addCookieItem(productId, action){
+    console.log('Not logged in..')
+
+    if (action == 'add') {
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action = 'remove') {
+        cart[productId]['quantity'] += 1
+
+        if (cart[productId]['quantity'] <= 0){
+            console.log('item should be deleted')
+            delete cart[productId];
+        }
+    }
+    console.log('cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+
+}
 
 function updateUserOrder(productId, action){
     console.log('user is logged in.')
